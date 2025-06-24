38cc26a4af4af8981a6330ec2e86791650a4f39b
999pryde <ma.munizaga@duocuc.cl>
2025-06-23 22:21:46 -0400

    llll

--- ActividadGIT.py
+++ ActividadGIT.py
@@ -1,21 +1,104 @@
 import os
+
 os.system("cls")
 
 
-def validar_lista_numeros(mensaje):
+
+nombre = ""
+
+listaNombre = [] 
+
+
+
+while True:
+
+
+
+  while True:
+
+    print ("1.- Agregar alumnos")
+
+    print ("2.- Mostrar todos los alumnos")
+
+    print ("3.- Eliminar alumno")
+
+    print ("4.- Salir")
+
+
+
+    try:
+
+      opcion = int(input("\nIngrese su eleccion (1/2/3/4): "))
+
+      if (1 <= opcion and opcion <=4):
+
+        break
+
+      else:
+
+        print("Por favor seleccione las opciones mostradas (1/2/3/4).")
+
+    except ValueError:
+
+      print("ERROR. Ingrese un dato valido de numero entero (1/2/3/4).")
+
+  
+
+  if opcion == 1:
+
     while True:
-        try:
-            lista_numeros = [int(num) for num in input(mensaje).split()]
-            
-            return lista_numeros
-        except ValueError:
-            print("Error: Ingrese una lista de números enteros válidos separados por espacios.")
+
+      nombre = input("\nIngrese su nombre: ")
+
+      if (nombre.isalpha()):
+
+        print("Su nombre se ingreso correctamente.")
+
+        listaNombre.append(nombre)
+
+        break
+
+      else:
+
+        print("\nIngrese su nombre correctamente: ") 
 
 
-numeros_ingresados = validar_lista_numeros("Ingrese una lista de números enteros separados por espacios: ")
 
-numeros_pares = [num for num in numeros_ingresados if num % 2 == 0]
-numeros_impares = [num for num in numeros_ingresados if num % 2 != 0]
+  if opcion == 2:
 
-print(f"Números pares: {numeros_pares}")
-print(f"Números impares: {numeros_impares}")
+    print (listaNombre)
+
+
+
+  if opcion == 3:
+
+    
+
+    while True:
+
+      try:
+
+        nombreEliminar = input("\nIngrese el nombre del alumno que desea eliminar: ")
+
+        if listaNombre == nombreEliminar:
+
+          listaNombre.remove(listaNombre)
+
+          print("Alumno eliminado con exito!.")
+
+          break
+
+        else:
+
+          print("Ingrese el Nombre correctamente.")
+
+      except ValueError:
+
+          print("ERROR.Ingrese el Nombre correctamente.")    
+
+
+
+  if opcion == 4:
+
+    break
+
